53fc1ddf9bb89bdc6671cdc96e0d72cbe5b7180a
Julien Nicolas <juli3nnicolas@gmail.com>
2016-01-26 13:31:27 +0100

    Demo

--- NeptunePracticeApp/NeptunePracticeApp/main.cpp
+++ NeptunePracticeApp/NeptunePracticeApp/main.cpp
@@ -26,24 +26,28 @@ int main(int argc, char* argv[])
 	TempFPSCameraController ctrl(&camera);
 	ctrl.init();
 
-	CubeFactory cf(1.0f, 1.0f, 0.0f);
-	View* view_table[100] = {0};
+	CubeFactory cf(0.1f, 0.3f, 0.3f);
+	const u8 OFFSET = 1;
+	//ModelFactory cf("Resources/Models/LowPolyTree.ply");
+	//const u8 OFFSET = 5;
+	const u8 NB_OBJECTS = 100;
+	View* view_table[NB_OBJECTS] = {0};
 
-	for (u8 i = 0; i < 100; i++)
+	for (u8 i = 0; i < NB_OBJECTS; i++)
 	{
 		view_table[i] = cf.create();
 		view_table[i]->init();
-		view_table[i]->getTransform().translate(-2*(i%2) + 1.0f, 0.0f, i);
+		view_table[i]->getTransform().translate(-2*OFFSET*(i%2) + 1.0f, 0.0f, i*OFFSET);
 		view_table[i]->bindToCamera(&camera);
 	}
 
-	float background[4] = { 0.0f,0.0f,0.0f,0.0f };
+	float background[4] = { 153.0f/255.0f,217.0f/255.0f,234.0f/255.0f,0.0f };
 	while(true)
 	{
 		DisplayDeviceInterface::ClearBuffers(background);
 		ctrl.update();
 		
-		for (u8 i = 0; i < 100; i++)
+		for (u8 i = 0; i < NB_OBJECTS; i++)
 			view_table[i]->update();
 
 		DisplayDeviceInterface::SwapBuffer(window);
