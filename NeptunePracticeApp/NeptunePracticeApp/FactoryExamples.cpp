353aa4cf1b193997a9db9603033ae50a6cec66a4
Julien Nicolas <juli3nnicolas@gmail.com>
2017-07-31 23:27:40 +0100

    added a new example with the ViewSpawner class. Works but there's still work remaining

---
+++ NeptunePracticeApp/NeptunePracticeApp/FactoryExamples.cpp
@@ -0,0 +1,73 @@
+#include "FactoryExamples.h"
+#include "Graphics/DisplayDeviceInterface.h"
+#include "Input/EventSystemInterface.h"
+
+#include "Graphics/Factories/CubeFactory.h"
+#include "Graphics/Factories/TriangleFactory.h"
+#include "Graphics/Factories/ModelFactory.h"
+
+#include "Graphics/Camera.h"
+#include "Math/Vectors/Vec3.h"
+
+#include "Debug/NeptuneDebug.h"
+
+#include "Camera/Controller/TempFPSCameraController.h"
+#include "Graphics/AmbientLight.h"
+
+using namespace Neptune;
+
+void FactoryExamples::ViewRows()
+{
+	DisplayDeviceInterface::WindowHandle window = DisplayDeviceInterface::CreateWindow("Test",1024,768);
+	DisplayDeviceInterface::GraphicalContextHandle ctxt = DisplayDeviceInterface::CreateGraphicalContext(window,3,4);
+	EventSystemInterface::StartUp();
+
+	AmbientLight ambientLight({1.0f,1.0f,1.0f,1.0f});
+
+	Camera camera;
+	camera.setScreenRatio(1024.0f/768.0f);
+	camera.translate(0.0f,0.0f,-5.0f);
+	TempFPSCameraController ctrl(&camera);
+	ctrl.init();
+
+	TriangleFactory cf({1.0f,1.0f,0.0f,1.0f});
+	//const u8 OFFSET = 1;
+	//ModelFactory cf("Resources/Models/LowPolyTree.ply");
+	const u8 OFFSET = 5;
+	const u8 NB_OBJECTS = 100;
+	View* view_table[NB_OBJECTS] ={0};
+
+	for(u8 i = 0; i < NB_OBJECTS; i++)
+	{
+		view_table[i] = cf.create();
+		view_table[i]->init();
+		view_table[i]->getTransform().translate(-2*OFFSET*(i%2) + 1.0f,0.0f,i*OFFSET);
+		view_table[i]->bindToCamera(&camera);
+	}
+
+	float background[4] ={0.0f/255.0f,0.0f/255.0f,0.0f/255.0f,0.0f};
+	while(true)
+	{
+		DisplayDeviceInterface::ClearBuffers(background);
+		ctrl.update();
+
+		for(u8 i = 0; i < NB_OBJECTS; i++)
+		{
+			view_table[i]->getTransform().rotate(0.0f,1.0f,0.0f);
+			view_table[i]->update();
+		}
+		DisplayDeviceInterface::SwapBuffer(window);
+	}
+
+	ctrl.terminate();
+	for(u8 i = 0; i < 100; i++)
+	{
+		view_table[i]->unbindFromCamera();
+		view_table[i]->terminate();
+		delete view_table[i];
+	}
+
+	EventSystemInterface::ShutDown();
+	DisplayDeviceInterface::DestroyWindow(window);
+	DisplayDeviceInterface::DestroyGraphicalContext(ctxt);
+}
--- NeptunePracticeApp/NeptunePracticeApp/main.cpp
+++ NeptunePracticeApp/NeptunePracticeApp/main.cpp
@@ -1,70 +1,92 @@
 #include "Graphics/DisplayDeviceInterface.h"
-#include "Input/EventSystemInterface.h"
 
-#include "Graphics/Factories/CubeFactory.h"
-#include "Graphics/Factories/TriangleFactory.h"
-#include "Graphics/Factories/ModelFactory.h"
-
-#include "Graphics/Camera.h"
+#include "Graphics/Factories/TriangleSpawner.h"
 #include "Math/Vectors/Vec3.h"
 
-#include "Debug/NeptuneDebug.h"
+#include "Graphics/Shader.h"
+#include "Graphics/Color.h"
+#include "Graphics/View.h"
+#include "Graphics/VAOView.h"
 
-#include "Camera/Controller/TempFPSCameraController.h"
-#include "Graphics/AmbientLight.h"
+#include <string>
+#include <chrono>
+#include <ratio>
+#include <ctime>
 
 using namespace Neptune;
+
+std::chrono::high_resolution_clock::time_point s_t_start = std::chrono::high_resolution_clock::now();
+
+namespace Example
+{
+	float GetCurrentTime()
+	{
+		using namespace std::chrono;
+
+		high_resolution_clock::time_point t_now   = high_resolution_clock::now();
+		duration<float> time_span                = duration_cast<duration<float>>(t_now - s_t_start);
+		
+		return time_span.count();
+	}
+}
 
 int main(int argc, char* argv[])
 {
 	DisplayDeviceInterface::WindowHandle window = DisplayDeviceInterface::CreateWindow("Test",1024,768);
 	DisplayDeviceInterface::GraphicalContextHandle ctxt = DisplayDeviceInterface::CreateGraphicalContext(window,3,4);
-	EventSystemInterface::StartUp();
+	
+	// Creates the graphics programs for the ViewSpawner
+	std::string vertexShaderName   = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Vertex/RawDisplay.vert";//FunWithUniforms.vert";
+	std::string fragmentShaderName = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Fragment/PassThrough.frag";
+	
+	Shader vert(vertexShaderName.c_str(),GL_VERTEX_SHADER);
+	Shader frag(fragmentShaderName.c_str(),GL_FRAGMENT_SHADER);
 
-	AmbientLight ambientLight({1.0f, 1.0f, 1.0f, 1.0f});
+	GraphicsProgram pgm;
+	pgm.add(vert.getId());
+	pgm.add(frag.getId());
 
-	Camera camera;
-	camera.setScreenRatio(1024.0f/768.0f);
-	camera.translate(0.0f, 0.0f, -5.0f);
-	TempFPSCameraController ctrl(&camera);
-	ctrl.init();
+	// Creates the uniforms
 
-	//CubeFactory cf( {0.8f, 0.5f, 0.0f, 1.0f} );
-	//const u8 OFFSET = 1;
-	ModelFactory cf("Resources/Models/LowPolyTree.ply");
-	const u8 OFFSET = 5;
-	const u8 NB_OBJECTS = 100;
-	View* view_table[NB_OBJECTS] = {0};
+	/*float current_time = Example::GetCurrentTime();
 
-	for (u8 i = 0; i < NB_OBJECTS; i++)
-	{
-		view_table[i] = cf.create();
-		view_table[i]->init();
-		view_table[i]->getTransform().translate(-2*OFFSET*(i%2) + 1.0f, 0.0f, i*OFFSET);
-		view_table[i]->bindToCamera(&camera);
-	}
+	GraphicsProgram::UniformVarInput current_time_uni("Time",
+		GraphicsProgram::FLOAT,
+		1,
+		1,
+		sizeof(float),
+		&current_time);*/
 
-	float background[4] = { 0.0f/255.0f,0.0f/255.0f,0.0f/255.0f,0.0f };
+	// Creates the spawner
+	const char PGM_NAME[] = "Bob";
+	TriangleSpawner spawner(PGM_NAME, &pgm);
+	spawner.createVertexData();
+	spawner.createColorData({0.5f, 0.2f, 0.3f, 1.0f});
+	spawner.mapVertexData(PGM_NAME, 0);
+	spawner.mapColorData(PGM_NAME, 1);
+	//spawner.addUniformVariable(PGM_NAME, current_time_uni);
+
+	// Create a view
+	View* view = spawner.create();
+	view->init();
+
+	// main loop
+	float background[4] ={0.0f/255.0f,0.0f/255.0f,0.0f/255.0f,0.0f};
 	while(true)
 	{
 		DisplayDeviceInterface::ClearBuffers(background);
-		ctrl.update();
 		
-		for (u8 i = 0; i < NB_OBJECTS; i++)
-			view_table[i]->update();
+		//current_time = Example::GetCurrentTime();
+		//view->getRenderer().updateUniform("Time", );
+		view->update();
 
 		DisplayDeviceInterface::SwapBuffer(window);
 	}
 
-	ctrl.terminate();
-	for(u8 i = 0; i < 100; i++)
-	{
-		view_table[i]->unbindFromCamera();
-		view_table[i]->terminate();
-		delete view_table[i];
-	}	
+	// Cleanup/shutdown
+	view->terminate();
+	delete view;
 
-	EventSystemInterface::ShutDown();
 	DisplayDeviceInterface::DestroyWindow(window);
 	DisplayDeviceInterface::DestroyGraphicalContext(ctxt);
 
