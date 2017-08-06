7cd5e3aff3761ac7eeeefc905726b86498055f34
Julien Nicolas <juli3nnicolas@gmail.com>
2017-08-06 02:39:47 +0100

    added a new example with a ViewSpawner and a world matrix. View class' update method needs to be amended

--- NeptunePracticeApp/NeptunePracticeApp/ViewSpawnerExamples.cpp
+++ NeptunePracticeApp/NeptunePracticeApp/ViewSpawnerExamples.cpp
@@ -1,3 +1,4 @@
+#include "ViewSpawnerExamples.h"
 #include "Graphics/DisplayDeviceInterface.h"
 
 #include "Graphics/Factories/TriangleSpawner.h"
@@ -7,27 +8,39 @@
 #include "Graphics/Color.h"
 #include "Graphics/View.h"
 #include "Graphics/VAOView.h"
-#include "Graphics/UniformVarNames.h"
-
-#include "Physics/Mechanics/Position.h"
 
 #include <string>
 #include <chrono>
 #include <ratio>
 #include <ctime>
 
+
 using namespace Neptune;
 
+std::chrono::high_resolution_clock::time_point s_t_start = std::chrono::high_resolution_clock::now();
 
-int main(int argc, char* argv[])
+namespace Example
+{
+	float GetCurrentTime()
+	{
+		using namespace std::chrono;
+
+		high_resolution_clock::time_point t_now   = high_resolution_clock::now();
+		duration<float> time_span                = duration_cast<duration<float>>(t_now - s_t_start);
+
+		return time_span.count();
+	}
+}
+
+void ViewSpawnerExamples::TimeControledTriangleColor()
 {
 	DisplayDeviceInterface::WindowHandle window = DisplayDeviceInterface::CreateWindow("Test",1024,768);
 	DisplayDeviceInterface::GraphicalContextHandle ctxt = DisplayDeviceInterface::CreateGraphicalContext(window,3,4);
-	
+
 	// Creates the graphics programs for the ViewSpawner
-	std::string vertexShaderName   = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Vertex/Display.vert";
+	std::string vertexShaderName   = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Vertex/FunWithUniforms.vert";
 	std::string fragmentShaderName = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Fragment/PassThrough.frag";
-	
+
 	Shader vert(vertexShaderName.c_str(),GL_VERTEX_SHADER);
 	Shader frag(fragmentShaderName.c_str(),GL_FRAGMENT_SHADER);
 
@@ -35,15 +48,25 @@ int main(int argc, char* argv[])
 	pgm.add(vert.getId());
 	pgm.add(frag.getId());
 
+	// Creates the uniforms
+
+	float current_time = Example::GetCurrentTime();
+
+	GraphicsProgram::UniformVarInput current_time_uni("Time",
+		GraphicsProgram::FLOAT,
+		1,
+		1,
+		sizeof(float),
+		&current_time);
+
 	// Creates the spawner
 	const char PGM_NAME[] = "Bob";
-	TriangleSpawner spawner(PGM_NAME, &pgm);
+	TriangleSpawner spawner(PGM_NAME,&pgm);
 	spawner.createVertexData();
-	spawner.createColorData({1.0f, 0.8f, 0.0f, 1.0f});
-	spawner.setWorldPosition({-0.5f, 0.25f, 0.0f});
-	spawner.mapVertexData(PGM_NAME, 0);
-	spawner.mapColorData(PGM_NAME, 1);
-	spawner.useWorldMatrix(PGM_NAME, NEP_UNIVNAME_MV_MATRIX);
+	spawner.createColorData({1.0f,0.8f,0.0f,1.0f});
+	spawner.mapVertexData(PGM_NAME,0);
+	spawner.mapColorData(PGM_NAME,1);
+	spawner.addUniformVariable(PGM_NAME,current_time_uni);
 
 	// Create a view
 	View* view = spawner.create();
@@ -54,7 +77,9 @@ int main(int argc, char* argv[])
 	while(true)
 	{
 		DisplayDeviceInterface::ClearBuffers(background);
-		
+
+		current_time = Example::GetCurrentTime();
+		view->getRenderer().updateUniform(0,"Time",&current_time);
 		view->update();
 
 		DisplayDeviceInterface::SwapBuffer(window);
@@ -66,6 +91,5 @@ int main(int argc, char* argv[])
 
 	DisplayDeviceInterface::DestroyWindow(window);
 	DisplayDeviceInterface::DestroyGraphicalContext(ctxt);
-
-	return 0;
 }
+
---
+++ NeptunePracticeApp/NeptunePracticeApp/ViewSpawnerExamples.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace ViewSpawnerExamples
+{
+	void TimeControledTriangleColor();
+}
+
