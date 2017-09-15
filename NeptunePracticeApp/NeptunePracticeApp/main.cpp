f3068a544203b72dc2335307728be390efcada5a
Julien Nicolas <juli3nnicolas@gmail.com>
2017-09-15 21:14:11 +0100

    Added an example with multi texturing

--- NeptunePracticeApp/NeptunePracticeApp/main.cpp
+++ NeptunePracticeApp/NeptunePracticeApp/main.cpp
@@ -40,24 +40,48 @@ int main(int argc, char* argv[])
 	pgm.add(vert.getId());
 	pgm.add(frag.getId());
 
-	// Create a Texture
-	Texture texture("Resources/Textures/Sonic.png");
-	texture.init();
+	// Create Textures
+	Texture grass_texture("Resources/Textures/Grass.png");
+	grass_texture.init();
+	grass_texture.setIndex(0);
+
+	Texture sonic_texture("Resources/Textures/SonicKTX.ktx");
+	sonic_texture.init();
+	sonic_texture.setIndex(0);
+
+	View* grass_square1 = nullptr, *grass_square2 = nullptr, *sonic = nullptr;
 
 	// Creates the spawner
-	const GraphicsProgram::ProgramName PGM_NAME = pgm.getName();
-	PlanSpawner spawner(&pgm);
-	spawner.createVertexData();
-	spawner.create2DTextureMapData();
-	spawner.setWorldPosition({-0.5f, -0.5f, 0.0f});
-	spawner.mapVertexData(PGM_NAME,0);
-	spawner.map2DTextureMapData(PGM_NAME, 1);
-	spawner.useWorldMatrix(PGM_NAME, NEP_UNIVNAME_MV_MATRIX);
+	// Test that it is effectively a proxy between views and their data
+	{
+		const GraphicsProgram::ProgramName PGM_NAME = pgm.getName();
+		PlanSpawner spawner(&pgm);
+		spawner.createVertexData();
+		spawner.create2DTextureMapData();
+		spawner.setWorldPosition({-0.5f, -0.5f, 0.0f});
+		spawner.mapVertexData(PGM_NAME,0);
+		spawner.map2DTextureMapData(PGM_NAME, 1);
+		spawner.useWorldMatrix(PGM_NAME, NEP_UNIVNAME_MV_MATRIX);
+		spawner.setTexture(PGM_NAME, &sonic_texture);
 
-	// Create a view
-	View* view = spawner.create();
-	view->bindTexture(&texture);
-	view->init();
+
+		// Create sonic
+		sonic = spawner.create();
+		sonic->init();
+
+		// Swap texture: all views will be rendered with grass_texture even sonic
+		spawner.setTexture(PGM_NAME, &grass_texture); // swaps the texture for all instances
+
+		// Create grass_square1
+		grass_square1 = spawner.create();
+		grass_square1->init();
+		grass_square1->getTransform().translate(-0.5f, -0.5f, 0.0f);
+
+		// Create grass_square2
+		grass_square2 = spawner.create();
+		grass_square2->init();
+		grass_square2->getTransform().translate(0.2f, -0.5f, 0.0f);
+	}
 
 	// main loop
 	float background[4] = {255.0f/255.0f,255.0f/255.0f,255.0f/255.0f,0.0f};
@@ -65,19 +89,30 @@ int main(int argc, char* argv[])
 	{
 		DisplayDeviceInterface::ClearBuffers(background);
 
-		view->update();
+		// Texture swap at update time to limit memory consumption
+		// The same program is used to render different objects
+		// only the textures are swapped.
+		pgm.setTexture(&grass_texture);
+		grass_square1->update();
+		grass_square2->update();
+		
+		pgm.setTexture(&sonic_texture);
+		sonic->update();
 
 		DisplayDeviceInterface::SwapBuffer(window);
 	}
 
 	// Cleanup/shutdown
-	view->terminate();
-	delete view;
+	grass_square1->terminate();
+	grass_square2->terminate();
+	sonic->terminate();
+
+	delete grass_square1;
+	delete grass_square2;
+	delete sonic;
 
 	DisplayDeviceInterface::DestroyWindow(window);
 	DisplayDeviceInterface::DestroyGraphicalContext(ctxt);
-
-	//ViewSpawnerExamples::ModelViewMatrix();
 
 	return 0;
 }
