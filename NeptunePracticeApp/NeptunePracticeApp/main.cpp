6b053412a7c30f9b0a653e2bcb18454fcf302b1e
Julien Nicolas <juli3nnicolas@gmail.com>
2017-09-16 18:06:32 +0100

    Added an example using the refactor of PlanFactory

--- NeptunePracticeApp/NeptunePracticeApp/main.cpp
+++ NeptunePracticeApp/NeptunePracticeApp/main.cpp
@@ -2,6 +2,7 @@
 
 #include "Graphics/Factories/TriangleSpawner.h"
 #include "Graphics/Factories/PlanSpawner.h"
+#include "Graphics/Factories/PlanFactory.h"
 #include "Math/Vectors/Vec3.h"
 
 #include "Graphics/Shader.h"
@@ -29,59 +30,16 @@ int main(int argc, char* argv[])
 	DisplayDeviceInterface::WindowHandle window = DisplayDeviceInterface::CreateWindow("Test",1024,768);
 	DisplayDeviceInterface::GraphicalContextHandle ctxt = DisplayDeviceInterface::CreateGraphicalContext(window,3,4);
 
-	// Creates the graphics programs for the ViewSpawner
-	std::string vertexShaderName   = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Vertex/BasicDisplay.vert";
-	std::string fragmentShaderName = "../../../Neptune/Engine/Multiplatform/Core/Shaders/Fragment/ApplyTexture.frag";
 
-	Shader vert(vertexShaderName.c_str(),GL_VERTEX_SHADER);
-	Shader frag(fragmentShaderName.c_str(),GL_FRAGMENT_SHADER);
+	Color color {1.0f, 0.0f, 0.0f, 1.0f};
+	//PlanFactory factory(color);
+	PlanFactory factory("Resources/Textures/Grass.png");
+	View* v1 = factory.create();
+	View* v2 = factory.create();
+	v1->init();
 
-	GraphicsProgram pgm("Bob");
-	pgm.add(vert.getId());
-	pgm.add(frag.getId());
-
-	// Create Textures
-	Texture grass_texture("Resources/Textures/Grass.png");
-	grass_texture.init();
-	grass_texture.setIndex(0);
-
-	Texture sonic_texture("Resources/Textures/SonicKTX.ktx");
-	sonic_texture.init();
-	sonic_texture.setIndex(0);
-
-	View* grass_square1 = nullptr, *grass_square2 = nullptr, *sonic = nullptr;
-
-	// Creates the spawner
-	// Test that it is effectively a proxy between views and their data
-	{
-		const GraphicsProgram::ProgramName PGM_NAME = pgm.getName();
-		PlanSpawner spawner(&pgm);
-		spawner.createVertexData();
-		spawner.create2DTextureMapData();
-		spawner.setWorldPosition({-0.5f, -0.5f, 0.0f});
-		spawner.mapVertexData(PGM_NAME,0);
-		spawner.map2DTextureMapData(PGM_NAME, 1);
-		spawner.useWorldMatrix(PGM_NAME, NEP_UNIVNAME_MV_MATRIX);
-		spawner.setTexture(PGM_NAME, &sonic_texture);
-
-
-		// Create sonic
-		sonic = spawner.create();
-		sonic->init();
-
-		// Swap texture: all views will be rendered with grass_texture even sonic
-		spawner.setTexture(PGM_NAME, &grass_texture); // swaps the texture for all instances
-
-		// Create grass_square1
-		grass_square1 = spawner.create();
-		grass_square1->init();
-		grass_square1->getTransform().translate(-0.5f, -0.5f, 0.0f);
-
-		// Create grass_square2
-		grass_square2 = spawner.create();
-		grass_square2->init();
-		grass_square2->getTransform().translate(0.2f, -0.5f, 0.0f);
-	}
+	v2->init();
+	v2->getTransform().translate(-0.6f, 0.0f, 0.0f);
 
 	// main loop
 	float background[4] = {255.0f/255.0f,255.0f/255.0f,255.0f/255.0f,0.0f};
@@ -89,27 +47,17 @@ int main(int argc, char* argv[])
 	{
 		DisplayDeviceInterface::ClearBuffers(background);
 
-		// Texture swap at update time to limit memory consumption
-		// The same program is used to render different objects
-		// only the textures are swapped.
-		pgm.setTexture(&grass_texture);
-		grass_square1->update();
-		grass_square2->update();
-		
-		pgm.setTexture(&sonic_texture);
-		sonic->update();
+		v1->update();
+		v2->update();
 
 		DisplayDeviceInterface::SwapBuffer(window);
 	}
 
 	// Cleanup/shutdown
-	grass_square1->terminate();
-	grass_square2->terminate();
-	sonic->terminate();
-
-	delete grass_square1;
-	delete grass_square2;
-	delete sonic;
+	v1->terminate();
+	v2->terminate();
+	delete v1;
+	delete v2;
 
 	DisplayDeviceInterface::DestroyWindow(window);
 	DisplayDeviceInterface::DestroyGraphicalContext(ctxt);
