bdeefb19020bb30a50c043331970ce1fa98d6713
Julien Nicolas <ju.nicodev@gmail.com>
2019-07-27 12:35:22 +0200

    improve cmake files... still not working

---
+++ CMake/BuildProjectTree.cmake
@@ -0,0 +1,11 @@
+# Scripts that creates the whole project's folder structure.
+# It is representative to where (the folders) code files are stored 
+# and keeps the same structure.
+
+cmake_minimum_required (VERSION 3.10)
+
+source_group("CMake" REGULAR_EXPRESSION "CMake/+\\w*")
+source_group("Src" REGULAR_EXPRESSION "Src/+\\w*")
+source_group("Resources" REGULAR_EXPRESSION "Resources/+\\w*")
+	source_group("Resources\\Shaders" REGULAR_EXPRESSION "Resources/+Shaders/+\\w*")
+		source_group("Resources\\Shaders\\SelfPractice" REGULAR_EXPRESSION "Resources/+Shaders/+SelfPractice/+\\w*")
